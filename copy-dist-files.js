19421702fb4aa59e862290c2172d787ad4e981cb
RequiemSoul <chrisluyi@gmail.com>
2017-05-26 16:38:01 +0800

    Add files via upload

---
+++ copy-dist-files.js
@@ -0,0 +1,11 @@
+﻿var fs = require('fs');
+var resources = [
+    'node_modules/core-js/client/shim.min.js',
+    'node_modules/zone.js/dist/zone.min.js',
+    'src/assets/style/all.css'
+];
+resources.map(function (f) {
+    var path = f.split('/');
+    var t = 'aot/' + path[path.length - 1];
+    fs.createReadStream(f).pipe(fs.createWriteStream(t));
+});
---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core';
+
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+    title = 'Hello World!';
+    selectedOption: string;
+    constructor() { }
+
+    onButtonClick() {
+        this.title = 'Hello from Kendo UI!';
+    }
+}
